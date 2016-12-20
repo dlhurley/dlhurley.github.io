b0eaae86fd204c4318186153da26954fc345198d
David Hurley <dlhurley@ncsu.edu>
2016-12-20 08:45:50 -0500

    Addition of images.

--- Extract.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash 
-
-curl "https://waterdata.usgs.gov/nwis/uv?site_no=02087182" >> out.txt
-
-awk 'FNR>=983&&FNR<=983' "out.txt" > temp.txt
-VAR="$(awk -F '[:.]' '{print $2}' temp.txt)"
-NOW=$(date +%b-%m-%Y)
-sed -e "s/%%Temp%%/$VAR/" -e "s/%%Date%%/$NOW/" FallsModel.html > FallsModelUP.html
-
